fa1a8a732ca08797a5523c4ca152d58a5f3e8aed
Amro Mentash <mentash@gmail.com>
2026-02-24 16:09:08 +0200

    Change Flask app port to 8080

--- app.py
+++ app.py
@@ -3,14 +3,17 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def hello():
     message = "Happy birthday {name}!"
-    return message.format(name=os.getenv('NAME', 'John'))
+    return message.format(name=os.getenv("NAME", "John"))
 
-@app.route('/about')
+
+@app.route("/about")
 def about():
-    return 'About page'
+    return "About page"
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=8080)
